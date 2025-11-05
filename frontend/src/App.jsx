160a5ad727f49ed3e49109da92bb0a9916df1f0f
Rushikesh <madasrushi900@gmail.com>
2025-11-05 12:33:13 +0530

    Create Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,15 @@ import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+import Footer from './components/fOOTER.JSX'
 
 
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/doctors' element={<Doctors/>}/>
@@ -22,11 +24,11 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
-        <Route path='/myprofile' element={<MyProfile/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointments' element={<MyAppointments/>}/>
         <Route path='/appointment/:docId' element={<Appointment/>}/>
-
       </Routes>
+      <Footer/>
     </div>
   )
 }
