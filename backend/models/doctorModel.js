3432d141a76fc5cf2b5f26cc0363b167983b0298
Rushikesh <madasrushi900@gmail.com>
2025-11-07 20:05:54 +0530

    Create MongoDB model to store Doctors Data and User Data

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+  name: {type:String, required:true},
+  email: {type:String, required:true,unique:true},
+  password: {type:String, required:true},
+  image: {type:String, required:true},
+  speciality: {type:String, required:true},
+  degree: {type:String, required:true},
+  experience: {type:String, required:true},
+  about: {type:String, required:true},
+  available: {type:String, required:true},
+  fees : {type:String, required:true},
+  address: {type:Object, required:true},
+  date: {type:Number, required:true},
+  slots_booked: {type:Object, default: {} },
+},{minimize:false}) 
+
+//Setting { minimize: false } in your schema options tells Mongoose:
+// “Hey, don’t remove empty objects — save them as they are.”
+
+const doctorModel = mongoose.models.doctor|| mongoose.model('doctor',doctorSchema)
+
+export default doctorModel;
