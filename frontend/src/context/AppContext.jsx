5dac108ba7d0ae910a76cc8b47a4b0d7d9714909
Rushikesh <madasrushi900@gmail.com>
2025-11-27 21:19:02 +0530

    Display and update patient profile data on website

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,25 +3,26 @@ import axios from 'axios'
 import { toast } from "react-toastify";
 export const AppContext = createContext()
 
-const AppContextProvider = (props) =>{
+const AppContextProvider = (props) => {
 
   const currencySymbol = '$'
   const backendUrl = import.meta.env.VITE_BACKEND_URL
-  const [doctors,setDoctors] = useState([])
 
-  const value = {
-    doctors,currencySymbol
-  }
+  const [doctors, setDoctors] = useState([])
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+  const [userData, setUserData] = useState(false)
+
+
 
   const getDoctorsData = async () => {
     try {
-      
-        const {data} = await axios.get(backendUrl+'/api/doctor/list')
-        if (data.success){
-            setDoctors(data.doctors)
-        }else {
-          toast.error(data.message)
-        }
+
+      const { data } = await axios.get(backendUrl + '/api/doctor/list')
+      if (data.success) {
+        setDoctors(data.doctors)
+      } else {
+        toast.error(data.message)
+      }
 
     } catch (error) {
       console.log(error)
@@ -29,9 +30,37 @@ const AppContextProvider = (props) =>{
     }
   }
 
-  useEffect(()=>{
+  const loadUserProfileData = async () => {
+    try {
+
+      const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+      if (data.success) {
+        setUserData(data.userData)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  const value = {
+    doctors, currencySymbol, token, setToken, backendUrl,userData,setUserData,loadUserProfileData
+  }
+
+  useEffect(() => {
     getDoctorsData()
-  },[])
+  }, [])
+
+  useEffect(()=>{
+    if (token){
+      loadUserProfileData()
+    } else {
+      setUserData(false)
+    }
+  })
 
   return (
     <AppContext.Provider value={value}>
