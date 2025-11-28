3a99fdbc8c6f9ff28eade5d9ffcedc51ff3444bd
Rushikesh <madasrushi900@gmail.com>
2025-11-28 14:04:48 +0530

    Create Appointment Cancellation Feature for Patient

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
@@ -6,38 +6,57 @@ import { useEffect } from 'react'
 
 const MyAppointments = () => {
 
-  const { backendUrl, token } = useContext(AppContext)
+  const { backendUrl, token, getDoctorsData } = useContext(AppContext)
 
-  const [appointments,setAppointments] = useState([])
-  const months = ["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+  const [appointments, setAppointments] = useState([])
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
   const slotDateFormat = (slotDate) => {
-      const dateArray = slotDate.split('_')
-      return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    const dateArray = slotDate.split('_')
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
   }
 
   const getUserAppointments = async () => {
     try {
-      
-      const {data} = await axios.get(backendUrl+'/api/user/appointments',{headers:{token}})
 
-      if(data.success){
+      const { data } = await axios.get(backendUrl + '/api/user/appointments', { headers: { token } })
+
+      if (data.success) {
         setAppointments(data.appointments.reverse())
-        console.log(data.appointments);
+
       }
 
     } catch (error) {
       console.log(error);
       toast.error(error.message)
-      
+
     }
   }
 
-  useEffect(()=>{
-    if(token){
+  const cancelAppointment = async (appointmentId) => {
+    try {
+
+      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment', { appointmentId }, { headers: { token } })
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointments()
+        getDoctorsData()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+
+  }
+
+  useEffect(() => {
+    if (token) {
       getUserAppointments()
     }
-  },[token])
+  }, [token])
 
   return (
     <div>
@@ -58,8 +77,9 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+              {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+              {item.cancelled && <button className='sm:min-w-48 py-2 border-red-500 rounded text-red-500' >Appointment Cancelled</button>}
             </div>
           </div>
         ))}
